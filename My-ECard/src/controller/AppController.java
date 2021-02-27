5fc0920bdcfe459a4d884f265d525283d75ce59d
Camposm <camposm1205@gmail.com>
2021-02-26 19:55:01 -0500

    Added More MenuItems

--- My-ECard/src/controller/AppController.java
+++ My-ECard/src/controller/AppController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import java.io.File;
+
 import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class AppController {
 	
-	
-	
+	public void importAsset(ActionEvent event) {
+		FileChooser fc = new FileChooser();
+		File file = fc.showOpenDialog(new Stage());
+		System.out.println(file);
+	}
 }
