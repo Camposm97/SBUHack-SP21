d86e2474a9ac43f7378e38cb1b81da306f183c15
Camposm <camposm1205@gmail.com>
2021-02-26 19:10:51 -0500

    LOOK FXML

---
+++ My-ECard/src/controller/AppController.java
@@ -0,0 +1,9 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class AppController {
+	public void buttonClicked(ActionEvent event) {
+		System.out.println("Hello World!");
+	}
+}
