9420982be20612dbfcd4d1049ecab6ea3a073a4c
Camposm <camposm1205@gmail.com>
2021-02-26 19:23:20 -0500

    Update App.java

--- My-ECard/src/app/App.java
+++ My-ECard/src/app/App.java
@@ -1,12 +1,19 @@
 package app;
 
+import java.io.File;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(new File("ui/App.fxml").toURI().toURL());
+		stage.setScene(new Scene(root));
 		stage.show();
 	}
 	
