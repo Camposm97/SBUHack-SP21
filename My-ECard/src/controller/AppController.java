1ae3d319cc20ef993568935229b7b45802b142cd
Camposm <camposm1205@gmail.com>
2021-02-26 20:47:48 -0500

    Added FXML Reference Field Example

--- My-ECard/src/controller/AppController.java
+++ My-ECard/src/controller/AppController.java
@@ -1,12 +1,17 @@
 package controller;
 
 import java.io.File;
+import java.util.Arrays;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class AppController {
+	@FXML
+	TextField tfName;
 	
 	public void importAsset(ActionEvent event) {
 		FileChooser fc = new FileChooser();
