f98627fb45cf2433525db755c3433e57b451c139
Camposm <camposm1205@gmail.com>
2021-02-27 13:11:01 -0500

    Organizing Code

--- My-ECard/src/util/DataUtil.java
+++ My-ECard/src/util/DataUtil.java
@@ -1,13 +1,15 @@
 package util;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import javafx.stage.FileChooser;
 import model.CardData;
 
-public class DataSaver {
+public class DataUtil {
 	public static void saveAs(CardData cardData) {
 		FileChooser fc = new FileChooser();
 	}
@@ -21,6 +23,22 @@ public class DataSaver {
 			oos.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public static CardData load(String src) {
+		File file = new File(src);
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			CardData cardData = (CardData) ois.readObject();
+			ois.close();
+			System.out.println("Successfully loaded from " + file);
+			return cardData;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Failed to load from " + file);
+			return null;
 		}
 	}
 }
