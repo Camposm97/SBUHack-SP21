33c948ace7ee68d862426a6ba87ecafa80fdaf53
Camposm <camposm1205@gmail.com>
2021-02-26 19:36:04 -0500

    Added MenuBar

--- My-ECard/src/app/App.java
+++ My-ECard/src/app/App.java
@@ -9,7 +9,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-
 	@Override
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(new File("ui/App.fxml").toURI().toURL());
