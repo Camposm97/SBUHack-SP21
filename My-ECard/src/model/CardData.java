b969e751a39d3ac7464fdf92e67ed69097f6e818
chrisdemonte <46972602+chrisdemonte@users.noreply.github.com>
2021-02-26 20:17:08 -0500

    Card Building
    Added image views and stock photos

---
+++ My-ECard/src/model/CardData.java
@@ -0,0 +1,14 @@
+package model;
+
+public class CardData {
+	
+	private String name;
+	private String message;
+	private String email;
+	private String address;
+	private String phone;
+	private String website;
+	
+	
+
+}
